26169966e999082be50a0d846c1148bdf963cf98
Ichi <924426118@qq.com>
2019-04-20 19:10:19 +0800

    Log增加文件输出，可以在logs文件夹中找到以往的日志了

--- src/core/ClientProxy.java
+++ src/core/ClientProxy.java
@@ -1,11 +1,12 @@
 package core;
 
 import client.frame.MainFrame;
+import log.Logger;
 import network.Side;
 
 public class ClientProxy extends Proxy {
 
-	public ClientProxy( ) {
+	public ClientProxy() {
 		super(Side.CLIENT);
 	}
 
@@ -15,11 +16,13 @@ public class ClientProxy extends Proxy {
 	@Override
 	public void init() {
 		super.init();
+		Logger.log.impart("正在初始化客户端...");
 	}
 
 	@Override
 	public void lunch() {
 		super.lunch();
+		Logger.log.impart("正在启动客户端...");
 		// 启动窗体
 		frame = new MainFrame();
 	}
--- src/log/Logger.java
+++ src/log/Logger.java
@@ -1,13 +1,70 @@
 package log;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Calendar;
 
 public class Logger {
 	/** 日志句柄 */
 	static public Logger log = new Logger();
+	/** 日志文件 */
+	private File logFile = null;
 
-	public Logger() {
+	/** 日志输出流 */
+	private PrintStream out = System.out;
 
+	private Logger() {
+		Calendar cal = Calendar.getInstance();
+		String folderPath = cal.get(Calendar.YEAR) + "-" + cal.get(Calendar.MONTH);
+		File folder = new File("./logs/" + folderPath);
+		if (!folder.exists())
+			folder.mkdirs();
+		String logPath = folderPath + "-" + cal.get(Calendar.DATE);
+		logFile = new File("./logs/" + folderPath + "/" + logPath + ".log");
+		try {
+			if (!logFile.exists())
+				logFile.createNewFile();
+		} catch (IOException e) {
+			logFile = null;
+			this.error("日志文件加载失败！", e);
+			return;
+		}
+		try {
+			out = new PrintStream(new FileOutputStream(logFile, true));
+		} catch (FileNotFoundException e1) {
+			out = System.out;
+			this.error("日志输出流加载失败！", e1);
+			return;
+		}
+		out.println("--------------------------" + this.getCurTime() + "--------------------------");
+		File fileLog = new File("./logs/last_log.log");
+		try {
+			if (!fileLog.exists())
+				fileLog.createNewFile();
+		} catch (IOException e) {
+			this.warn("日志last文件加载失败！", e);
+		}
+		PrintStream lastOut = null;
+		try {
+			lastOut = new PrintStream(new FileOutputStream(fileLog));
+			lastOut.print("最后一次记录的日志文件处于  " + logFile.getPath());
+		} catch (IOException e) {
+			this.warn("日志last文件写入失败！", e);
+		} finally {
+			if (lastOut != null)
+				lastOut.close();
+		}
+
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		super.finalize();
+		if (out != null)
+			out.close();
 	}
 
 	/** 获取当前时间 */
@@ -19,16 +76,20 @@ public class Logger {
 
 	/** 显示错误的轨迹 */
 	private void print(Throwable e) {
+		e.printStackTrace(out);
 		e.printStackTrace();
 	}
 
 	/** 打印 */
 	private void print(String str) {
+		out.println(this.getCurTime() + str);
 		System.out.println(this.getCurTime() + str);
 	}
 
 	/** 打印 */
 	private void print(String format, Object... objs) {
+		out.printf(this.getCurTime() + format, objs);
+		out.println();
 		System.out.printf(this.getCurTime() + format, objs);
 		System.out.println();
 	}
