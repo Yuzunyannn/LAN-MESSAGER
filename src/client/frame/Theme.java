919f07d3d742627a84bc4ceea9528dbc8c74cfb0
dispute <dispute@192.168.1.102>
2019-04-23 14:50:16 +0800

    界面基本样式

--- src/client/frame/Theme.java
+++ src/client/frame/Theme.java
@@ -4,12 +4,14 @@ import java.awt.Color;
 
 public class Theme {
 
-	/** 主题的默认颜色 */
+	/** 主题的默认颜色 白色 */
 	public static Color COLOR0 = Color.WHITE;
-	/** 主题的第一种颜色 */
+	/** 主题的第一种颜色 蓝白色*/
 	public static Color COLOR1 = new Color(0xe5f1fa);
-	/** 主题的第二种颜色 */
+	/** 主题的第二种颜色 蓝灰色*/
 	public static Color COLOR2 = new Color(0xaed6f1);
-	/** 主题的第三种颜色 */
+	/** 主题的第三种颜色 蓝紫色*/
 	public static Color COLOR3 = new Color(0x4876FF);
+	/**	用户栏的颜色 		淡蓝紫色*/
+	public static Color COLOR4= new Color(0x7cbcf5);
 }
--- src/client/frame/info/InfoPanel.java
+++ src/client/frame/info/InfoPanel.java
@@ -1,5 +1,12 @@
 package client.frame.info;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Label;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import client.frame.Theme;
@@ -8,8 +15,32 @@ import client.frame.Theme;
 public class InfoPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-
 	public InfoPanel() {
 		this.setBackground(Theme.COLOR2);
+		this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		UserPanel c1=new UserPanel();
+		SearchPanel c2=new SearchPanel();
+		ListScrollPanel c3=new ListScrollPanel();
+		this.add(c1);
+		this.add(c2);
+		this.add(c3);
+		c3.addNewMember("1");
+		c3.addNewMember("1");
+		c3.addNewMember("1");
+		c3.addNewMember("1");
+		c3.addNewMember("1");
+		c3.addNewMember("1");
+		c3.addNewMember("1");
+		c3.addNewMember("1");
+		c3.addNewMember("1");
+		c3.addNewMember("1");
+		c3.addNewMember("1");
+		c3.addNewMember("1");
+		c3.addNewMember("1");
+//		this.add(c2,BorderLayout.CENTER);
+		c1.setPreferredSize(new Dimension(0,80));
+		c2.setPreferredSize(new Dimension(0,50));
+		
+
 	}
 }
