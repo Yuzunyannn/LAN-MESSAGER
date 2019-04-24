b0e61e197042f17315b03b5b9b5b44676b5aea5f
dispute <767110621@qq.com>
2019-04-24 12:03:40 +0800

    文本框默认文字，聚焦后消失

--- src/client/frame/info/SearchPanel.java
+++ src/client/frame/info/SearchPanel.java
@@ -1,7 +1,10 @@
 package client.frame.info;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.TextField;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -24,14 +27,43 @@ public class SearchPanel extends JPanel {
 		JButton set = new JButton();
 		this.add(b1);
 		this.add(set);
-
+		String info="单行输入";
+		
+		search.setForeground(Color.GRAY);
 		search.setSize(150, 20);
 		search.setLocation(5, 8);
-		search.setText("单行输入");
+		search.setText(info);
+		search.addFocusListener(new DefaultFocusListener(info,search));
 		b1.setSize(20, 20);
 		b1.setLocation(180, 8);
 		set.setSize(20, 20);
 		set.setLocation(220, 8);
 
+	}
+}
+class DefaultFocusListener implements FocusListener{
+	private String info;
+	private TextField textfield;
+	public DefaultFocusListener(String info,TextField textfield) {
+		this.info=info;
+		this.textfield=textfield;
+	}
+	@Override
+	public void focusGained(FocusEvent e) {
+		String temp=textfield.getText();
+		if(temp.equals(info)) {
+			textfield.setText(null);
+			textfield.setForeground(Color.BLACK);
+		}
+	}
+
+	@Override
+	public void focusLost(FocusEvent e) {
+		String temp=textfield.getText();
+		if(temp.equals("")) {
+			textfield.setForeground(Color.GRAY);
+			textfield.setText(info);
+		}
+		
 	}
 }
