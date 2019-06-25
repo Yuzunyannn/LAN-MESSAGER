62a9b4329c5eb0aa29320d31e36572938daf5397
dispute <767110621@qq.com>
2019-06-25 15:20:29 +0800

    megre

--- src/client/frame/info/SearchPanel.java
+++ src/client/frame/info/SearchPanel.java
@@ -3,7 +3,6 @@ package client.frame.info;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.TextField;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
