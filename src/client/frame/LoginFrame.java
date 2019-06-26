d852c2d5fe401143940d7584efd74b880203cde1
14787 <14787@DESKTOP-HI25L0N>
2019-06-26 16:26:19 +0800

    试试

--- src/client/frame/LoginFrame.java
+++ src/client/frame/LoginFrame.java
@@ -79,11 +79,11 @@ public class LoginFrame extends JFrame {
 		// 颜色
 		this.getContentPane().setBackground(Theme.COLOR0);
 		// 账号框
-		userNameText = new JTextField("ssj");
+		userNameText = new JTextField("ycy");
 		userNameText.setBorder(BorderFactory.createLineBorder(Theme.COLOR6));
 		this.add(userNameText);
 		// 密码框
-		password = new JPasswordField("123");
+		password = new JPasswordField("tatsuu");
 		password.setBorder(BorderFactory.createLineBorder(Theme.COLOR6));
 		this.add(password);
 		// 信息提示
